972908a7ed5f7bcd6ffca76efe168ab6a55a479d
AJ <ancheetah@users.noreply.github.com>
2021-05-04 02:43:51 -0400

    UI

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Input, Icon } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Input placeholder="Enter a task"
+             rightIcon={ <Icon name='add-circle' size={26} color='gray' /> }
+      />
     </View>
   );
 }
